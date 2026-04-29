979c20e0d2c23c95b8549a109d6f521e2b126abd
Mücahit Genç <105339374+EH-MucahitGenc@users.noreply.github.com>
2026-04-29 15:49:02 +0300

    eski blazor uygulaması kladırıldı

--- FormfleksBaseApp.Web/Models/ApiCallResult.cs
+++
@@ -1,9 +0,0 @@
-namespace FormfleksBaseApp.Web.Models;
-
-public sealed class ApiCallResult<T>
-{
-    public bool Success { get; set; }
-    public T? Data { get; set; }
-    public string? Error { get; set; }
-    public int? StatusCode { get; set; }
-}
